32bfa96c07ecc8326acca3914021ad7bddcab043
zfx <zfx@jshxlsp.com>
2023-02-28 23:16:52 +0800

    feat: 初始化项目

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  singleQuote: true,
+  semi: false,
+  useTabs: true,
+  tabWidth: 2,
+}
